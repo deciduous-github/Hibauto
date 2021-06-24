3ccaae38e4a460329ee569196c10b3cf56fb53d6
Decidupus_github <hayabusa.yuto.1212@gamil.com>
2021-06-24 22:54:09 +0900

    main.py
    icalを読み込み、CSVfileに変換するコードを書きました。
    dataframe.pyでpandasをつかってdataを変換するコードを作る予定です。

--- ical/main.py
+++ ical/main.py
@@ -1,22 +1,31 @@
 import codecs
 from icalendar import Calendar
 
-def flatten_2d(data):
-    for block in data:
-        for elem in block:
-            yield elem
-
 ical_pass = "iCalender.ics"
-fin = codecs.open(ical_pass)
-cal = Calendar.from_ical(fin.read())
-fin.close()
-for ev in cal.walk():
-    if ev.name == 'VEVENT':
-        start_dt = ev.decoded("dtstart")
-        end_dt = ev.decoded("dtend")
-        summary = ev['summary'].encode('utf-8')
-        summary2 = summary.decode().replace('\u3000', '').split(" ")
-        # 半角スペースで区切りたい
-        print("{start} - {end} : {summary}".format(start=start_dt.strftime("%Y/%m/%d"),
-                                                   end=end_dt.strftime("%Y/%m/%d"),
-                                                   summary=summary2))
+
+def ical():
+    fin = codecs.open(ical_pass)
+    cal = Calendar.from_ical(fin.read())
+    fin.close()
+    li = []
+    for ev in cal.walk():
+        if ev.name == 'VEVENT':
+            start_dt = ev.decoded("dtstart")
+            end_dt = ev.decoded("dtend")
+            summary = ev['summary'].encode('utf-8')
+            summary2 = summary.decode().replace('\u3000', '')
+            summary3 = ','.join([x.strip() for x in summary2.split(' ')])
+            # todo 別ファイルに例外処理をまとめるファイルを作りたい。.replace('\u3000', '')
+            li.append("{start},{end},{summary}".format(start=start_dt.strftime("%Y/%m/%d"),
+                                                       end=end_dt.strftime("%Y/%m/%d"),
+                                                       summary=summary3))
+
+    ical_csv_pass = "ical.csv"
+    f = codecs.open(ical_csv_pass, "w")
+    for ld in li:
+        f.write(ld + "\n")
+    f.close()
+
+if __name__ == '__main__':
+    ical()
+    print("mainで実行中")
